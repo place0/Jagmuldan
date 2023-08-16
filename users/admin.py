3dda5a0f8d9080a3497b7d6daef3f8c668fd07a5
7beunseo <kimes0403@gmail.com>
2023-08-16 22:15:14 +0900

    upload, basket, rp 수정

--- users/admin.py
+++ users/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Merchant)
+class PhotoInline(admin.TabularInline):
+    model = Photo
 
-admin.site.register(Restaurant)
+class RestaurantModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')  # 이 부분은 Restaurant 모델의 필드들을 적절하게 추가하셔야 합니다.
+    search_fields = ('name',)
+    inlines = [PhotoInline]
+
+admin.site.register(Merchant)
+admin.site.register(Restaurant, RestaurantModelAdmin)  # RestaurantModelAdmin을 사용하도록 등록
 admin.site.register(User)
 admin.site.register(LowIncome)
-
