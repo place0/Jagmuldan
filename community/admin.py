099e2301581d6dbb9063cec1d50fdfc92fee595a
7beunseo <kimes0403@gmail.com>
2023-08-18 23:08:31 +0900

    배포 수정본

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Comment, JointShipping
 
-# Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+class JointShippingAdmin(admin.ModelAdmin):
+    list_display = ('context',)  # 필요한 필드들을 추가하십시오.
+    inlines = [CommentInline]  # CommentInline을 JointShipping 모델에 인라인으로 추가
+
+admin.site.register(JointShipping, JointShippingAdmin)
